fa8064afc6079f1db8492a357e75989681d1dec2
jinhwan7 <rkflql851@gmail.com>
2024-09-29 14:28:57 +0900

    요청,응답시 적절한 response,request 객체로 변경해서 주고 받도록 수정

--- src/main/java/com/reusable_server/reusableServer/member/presentation/MemberController.java
+++ src/main/java/com/reusable_server/reusableServer/member/presentation/MemberController.java
@@ -2,23 +2,24 @@ package com.reusable_server.reusableServer.member.presentation;
 
 import com.reusable_server.reusableServer.common.dto.ApiResponse;
 import com.reusable_server.reusableServer.common.enums.ReturnCode;
-import com.reusable_server.reusableServer.member.presentation.dtos.MemberItemResponse;
+import com.reusable_server.reusableServer.member.application.dtos.MemberCreateParam;
+import com.reusable_server.reusableServer.member.application.dtos.MemberUpdateParam;
+import com.reusable_server.reusableServer.member.presentation.dtos.response.MemberItemResponse;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 import com.reusable_server.reusableServer.member.application.MemberService;
 import com.reusable_server.reusableServer.member.domain.Member;
-import com.reusable_server.reusableServer.member.presentation.dtos.MemberCreateDTO;
-import com.reusable_server.reusableServer.member.presentation.dtos.MemberUpdateDto;
-import com.reusable_server.reusableServer.member.presentation.response.MemberCreateReponse;
-import com.reusable_server.reusableServer.member.presentation.response.MemberFindAllResponse;
-import com.reusable_server.reusableServer.member.presentation.response.MemberFindOneResponse;
-import com.reusable_server.reusableServer.member.presentation.response.MemberUpdateResponse;
+import com.reusable_server.reusableServer.member.presentation.dtos.request.MemberCreateRequest;
+import com.reusable_server.reusableServer.member.presentation.dtos.response.MemberListResponse;
+import com.reusable_server.reusableServer.member.presentation.dtos.response.MemberSignupResponse;
+import com.reusable_server.reusableServer.member.presentation.dtos.request.MemberUpdateRequest;
+import com.reusable_server.reusableServer.member.presentation.dtos.response.MemberUpdateResponse;
 
 @RestController
 @RequestMapping("/api/members")
@@ -28,15 +29,22 @@ public class MemberController {
 	private final MemberService memberService;
 
 	@PostMapping
-	public ApiResponse<?> signup(@Valid @RequestBody MemberCreateDTO memberCreateDTO) {
-		Member createdMember = memberService.createMember(memberCreateDTO);
-		return ApiResponse.of(ReturnCode.SUCCESS);
+	public ApiResponse<?> signup(@Valid @RequestBody MemberCreateRequest memberCreateDTO) {
+
+		//memberCreateDTO를 MemberCreateParam(컨트롤러->서비스)
+		MemberCreateParam memberCreateParam = MemberCreateParam.from(memberCreateDTO);
+
+		Member createdMember = memberService.createMember(memberCreateParam);
+
+		MemberSignupResponse memberSignupResponse = MemberSignupResponse.of(createdMember);
+		return ApiResponse.of(memberSignupResponse);
+
 		// TODO: DTO 변환 필요
-//		return MemberCreateReponse.success(
-//			createdMember.getId(),
-//			createdMember.getName(),
-//			createdMember.getEmail()
-//		);
+		//		return MemberCreateReponse.success(
+		//			createdMember.getId(),
+		//			createdMember.getName(),
+		//			createdMember.getEmail()
+		//		);
 	}
 
 	@GetMapping("/{id}")
@@ -49,15 +57,19 @@ public class MemberController {
 	@GetMapping
 	public ApiResponse<?> findAll() {
 		List<Member> members = memberService.findAll();
+		MemberListResponse memberListResponse = MemberListResponse.of(members);
 		// TODO: DTO로 변환 필요
-		return ApiResponse.of(members);
+		return ApiResponse.of(memberListResponse);
 	}
 
 	@PutMapping("/{id}")
-	public  ApiResponse<?> updateMember(@PathVariable Long id,
-		@Valid @RequestBody MemberUpdateDto memberUpdateDto) {
-		Member updatedMember = memberService.updateMember(id, memberUpdateDto);
-		return ApiResponse.of(updatedMember);
+	public ApiResponse<?> updateMember(@PathVariable Long id,
+		@Valid @RequestBody MemberUpdateRequest memberUpdateRequest) {
+		MemberUpdateParam memberUpdateParam = MemberUpdateParam.from(memberUpdateRequest);
+		Member updatedMember = memberService.updateMember(id, memberUpdateParam);
+		MemberUpdateResponse memberUpdateResponse = MemberUpdateResponse.of(updatedMember);
+
+		return ApiResponse.of(memberUpdateResponse);
 	}
 
 	@DeleteMapping("/{id}")
